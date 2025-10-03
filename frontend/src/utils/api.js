9063151e32d6d08c95f63a34e50f99c7c714ae6e
arfanwibowo <arfan.wibowo@geekseat.com.aui>
2025-10-03 20:46:38 +0700

    update vercel

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:5000'
+const API_BASE_URL = import.meta.env.PROD
+  ? 'https://mileapp-fullstack-production.up.railway.app'
+  : 'http://localhost:5000'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
